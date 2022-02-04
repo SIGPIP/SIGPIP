233eaa91c5ae6f7890543f4c5e4459eb6a1852d3
Johan15111 <jolemusp@unal.edu.co>
2022-02-04 10:49:19 -0500

    Changes in the frontend of the projects, validation of fields

---
+++ SIGPIP/Migrations/20220129204333_DatabaseMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SIGPIP.Migrations
+{
+    public partial class DatabaseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
